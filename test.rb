50de9ee5f9dac40e8d008d32533ef53cf69e1ae5
ghbccoding <ghbccoding@gmail.com>
2015-05-24 13:40:51 -0400

    Class birds examples

--- test.rb
+++ test.rb
@@ -1,4 +1,22 @@
-a = 1
+class Bird
+	@@count = 0
+	attr_accessor :name
+	
+	def initialize(name)
+		@name = name
+		self.add_one
+	end
 
-puts "This is version with %d\n" % a
-puts "This is string version #{a.to_s}"
+	def add_one
+		@@count += 1
+	end
+
+	def how_many_other_birds
+		@@count - 1
+	end
+end
+
+robin = Bird.new("Robin")
+cardinal = Bird.new("Cardinal")
+tweety = Bird.new("Tweety")
+puts "#{tweety.name} sees #{tweety.how_many_other_birds.to_s} other birds"
